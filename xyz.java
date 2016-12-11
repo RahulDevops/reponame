e55496b03f545093b6c03e458a926386c2e5ed17
RahulDevops <goud.rahul00@gmail.com>
2016-12-11 12:51:13 +0530

    added a bit of other info in xyz.java

--- xyz.java
+++ xyz.java
@@ -1,4 +1,7 @@
 this is Rahul 
 we re working in Git hub 
 practicing and creating a java file right now 
-
+So now I have created all these files and now here 
+I'm gonna add few other info iin this 
+so lets see how it works now
+:
